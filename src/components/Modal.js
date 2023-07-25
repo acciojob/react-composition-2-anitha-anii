d21ff962be3b78699283c81059884e38483c4202
anitha-anii <anithachowdary355@gmail.com>
2023-07-25 10:47:54 +0530

    'changes'

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.modalRef = React.createRef();
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  handleClickOutside = (event) => {
+    if (this.modalRef.current && !this.modalRef.current.contains(event.target)) {
+      this.props.onClose();
+    }
+  };
+
   render() {
     const { show, onClose, children } = this.props;
 
@@ -10,11 +29,11 @@ class Modal extends React.Component {
 
     return (
       <div className="model-overlay">
-        <div className="model-dialog">
+        <div className="model-dialog" ref={this.modalRef}>
           <button className="model-close" onClick={onClose}>
             Close
           </button>
-          <p className="model p">{children}</p>
+          <p className="model-p">{children}</p>
         </div>
       </div>
     );
