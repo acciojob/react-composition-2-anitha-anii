c499baa148e405d9d2afa38be752c1d77b3d47ff
anitha-anii <anithachowdary355@gmail.com>
2023-07-25 10:18:23 +0530

    'changes'

---
+++ src/components/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Modal extends React.Component {
+  render() {
+    const { show, onClose, children } = this.props;
+
+    if (!show) {
+      return null;
+    }
+
+    return (
+      <div className="model-overlay">
+        <div className="model-dialog">
+          <button className="model-close" onClick={onClose}>
+            Close
+          </button>
+          <p className="model-p">{children}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
